a3616ed023a7bd0150a245428e8d1398de742c62
Belliato Riccardo <belliatoriccardo@gmail.com>
2022-11-11 10:56:19 +0000

    correct models

--- polls/models/__init__.py
+++ polls/models/__init__.py
@@ -1,1 +1,3 @@
 from .model import Question
+from .model import Choice
+from .model import Answer
--- polls/models/model.py
+++ polls/models/model.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-
 class Question(models.Model):
-    question_text = models.TextField
+    question_text = models.TextField()
 
-class Choices(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField
+    choice_text = models.CharField(max_length=50)
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.ForeignKey(Choices, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
