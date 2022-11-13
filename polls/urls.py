7d33111858e75f4c5d5ddfa4c45381b2634512cf
Simone Tomada <simonetomada@gmail.com>
2022-11-13 18:37:45 +0000

    Esempio risultati

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from . import views
+from .views.result_view import displayResult
+
 
 app_name = 'polls'
 urlpatterns = [
-    path('vote/<int:id>/', views.VoteView.as_view(), name = "vote")
+    path('vote/<int:id>/', views.VoteView.as_view(), name = "vote"),
+    path('result/<int:id>/', displayResult, name = "result")
 ]
