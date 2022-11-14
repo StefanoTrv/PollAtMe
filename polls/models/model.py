6ab197293f185c2dcd09384705b2efc15e419cf9
Belliato Riccardo <belliatoriccardo@gmail.com>
2022-11-14 19:54:54 +0000

    vote __str__ method, result_view improvements

--- polls/models/model.py
+++ polls/models/model.py
@@ -13,3 +13,6 @@ class Choice(models.Model):
 class Vote(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"({self.question.id}, {self.choice.id})"
--- polls/views/result_view.py
+++ polls/views/result_view.py
@@ -4,15 +4,16 @@ from typing import *
 from django.http import HttpRequest, HttpResponse
 from django.db.models import Count
 from django.shortcuts import render
-
+from django.db.models.query import QuerySet
 
 from polls.models import Vote, Choice, Question
 
 #quick and dirty
 def displayResult(request, *args, **kwargs) -> HttpResponse:
     #dobbiamo andare a prendere la risposta
-    voti = Vote.objects.filter(question__exact = kwargs["id"]).values('choice').annotate(count = Count('choice')).order_by('-count')
-    return render(request, "result.html", {'risultati' : voti})
+    voti: QuerySet[Vote] = Vote.objects.filter(question = kwargs["id"])
+    count = voti.values('choice').annotate(count = Count('choice')).order_by('-count')
+    return render(request, "result.html", {'risultati' : voti, 'count': count})
 
 
 
