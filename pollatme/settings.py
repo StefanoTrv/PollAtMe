d018233621e8863b3a2cdfecec0085c47a241cc1
Riccardo <135047@spes.uniud.it>
2022-11-25 13:41:14 +0000

    Preparazione per la pull request in dev

--- pollatme/settings.py
+++ pollatme/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-tw(*s1_&$8oa)19y(fu)uyc^)yzs6h4^7fz*ny0ezbp@bker@d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS: list[str] = ['*']
 
@@ -121,7 +121,7 @@ USE_TZ = bool(environ.get('USE_TZ', True))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
