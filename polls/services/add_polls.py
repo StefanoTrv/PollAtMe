57836a8487d69b16f68c9c93ff6faca679ce6161
StefanoTrv <stefanotravasci@gmail.com>
2022-11-29 20:45:41 +0100

    Aggiunto servizio per aggiunta di sondaggi con Majority Judgment Aggiunto servizio per l'aggiunta nel db di sondaggi con metodo di giudizio maggioritario, con test.

--- polls/services/add_polls.py
+++ polls/services/add_polls.py
@@ -1,7 +1,10 @@
-from polls.models import Poll, SinglePreferencePoll, Alternative
+from polls.models import Poll, SinglePreferencePoll, MajorityOpinionPoll, Alternative
 
 def add_single_preference_poll(title : str, text : str, alternatives : list) -> None:
     __add_generic_poll(SinglePreferencePoll(),title,text,alternatives)
+
+def add_majority_judgment_poll(title : str, text : str, alternatives : list) -> None:
+    __add_generic_poll(MajorityOpinionPoll(),title,text,alternatives)
 
 def __add_generic_poll(poll : Poll, title : str, text : str, alternatives : list) -> None:
     poll.title = title[:100]
