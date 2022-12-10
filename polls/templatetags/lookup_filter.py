83cb4f0b9dd0fb6391e0b95eac576926ee75fcc5
Simone Tomada <simonetomada@gmail.com>
2022-12-10 10:27:35 +0000

    Miglioramenti al grafico a barre

--- polls/templatetags/lookup_filter.py
+++ polls/templatetags/lookup_filter.py
@@ -3,3 +3,6 @@ from django.template.defaulttags import register
 @register.filter
 def get_value(dictionary, key):
     return dictionary.get(key)
+@register.filter
+def replace_space_with_underscore(string):
+    return string.replace(" ", "_")
