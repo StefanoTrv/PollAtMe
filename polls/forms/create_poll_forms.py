21855866e3b77bfb7a0c11dfb871502e6ddd6d42
Riccardo <135047@spes.uniud.it>
2022-12-14 22:09:23 +0000

    favicon, creazione + riepilogo sondaggi

--- polls/forms/create_poll_forms.py
+++ polls/forms/create_poll_forms.py
@@ -2,12 +2,12 @@ from django import forms
 
 #Form per la pagina principale della pagina di creazione di nuovi sondaggi, contenente i dati principali
 class CreatePollFormMain(forms.Form):
-    poll_title = forms.CharField(label = 'Titolo', max_length=100)
+    poll_title = forms.CharField(label = 'Titolo', max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Inserisci il titolo del sondaggio'}))
     poll_type = forms.ChoiceField(label = 'Tipo di sondaggio', choices=[
         ('Giudizio maggioritario', 'Giudizio maggioritario'),
         ('Preferenza singola', 'Preferenza singola'),
-    ])
-    poll_text = forms.CharField(label = 'Testo', widget=forms.Textarea)
+    ], widget=forms.Select(attrs={'class':'form-control'}))
+    poll_text = forms.CharField(label = 'Testo', widget=forms.Textarea(attrs={'class':'form-control', 'rows':'4', 'placeholder':'Inserisci il testo della domanda del sondaggio'}))
     hidden_alternative_count = forms.IntegerField(widget=forms.HiddenInput())#sia quelle attive che quelle che l'utente ha cancellato, ma che in realtà sono solo nascoste
 
     def __init__(self, *args, **kwargs):
@@ -28,7 +28,7 @@ class CreatePollFormMain(forms.Form):
 
         for index in range(max(number_of_alternatives,len(alternatives))):
             # generate extra fields in the number specified via hidden_alternative_count and enough for all the alternatives passed as input (that is, the max of the two)
-            self.fields['alternative'+str(index+1)] = forms.CharField(label = 'Alternativa', max_length=100, required=False)
+            self.fields['alternative'+str(index+1)] = forms.CharField(label = 'Alternativa', max_length=100, required=False, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Inserisci il testo dell\'alternativa'}))
         
         for index in range(len(alternatives)):
             self.fields['alternative'+str(index+1)].initial=alternatives[index]
