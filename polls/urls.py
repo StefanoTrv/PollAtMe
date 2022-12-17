f419ccef242b0859173bc7a65d08ec4412c4a4a4
StefanoTrv <stefanotravasci@gmail.com>
2022-12-17 15:38:01 +0100

    Revert "delete view"
    This reverts commit 466a03edf5899c987bd1e1b7fd96d5f6951e0763.
    La modifica rompe la cancellazione.

--- polls/urls.py
+++ polls/urls.py
@@ -8,9 +8,8 @@ urlpatterns = [
     path('vote/<int:id>/', views.VotingView.as_view(), name = 'vote'),
     path('result/<int:id>/', views.ResultView.as_view(), name = 'result'),
     path('', views.IndexView.as_view(), name='index'),
-    path('create_poll/', views.create_poll, name='create_poll'),
-    path('delete_poll/<int:pk>', views.PollDeleteView.as_view(), name='delete_poll'),
-    path('edit_poll/<int:id>', views.edit_poll, name='edit_poll')
+    path('edit_poll/<int:id>', views.edit_poll, name='edit_poll'),
+    path('create_poll/', views.create_poll, name='create_poll')
 ]
 
 handler403 = 'polls.views.error_pages.error_403'
