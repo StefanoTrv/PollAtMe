49d52c69932299a46abbe35ddcc0d21faf2ad3a2
Riccardo Belliato <belliatoriccardo@gmail.com>
2022-12-23 23:18:38 +0000

    pagination

--- polls/services/active_polls.py
+++ polls/services/active_polls.py
@@ -1,14 +1,12 @@
-from polls.models import Poll, SinglePreferencePoll, MajorityOpinionPoll, Alternative
+from polls.models import Poll
 from django.db.models import Count
 from polls.exceptions import PollWithoutAlternativesException
-from django.db.models import QuerySet
 
 class ActivePollsService:
     """Service for get all active polls from database"""
 
     def __init__(self) -> None:
-        self.__single_preference_polls = SinglePreferencePoll.objects.annotate(num_alternatives=Count('alternative')).filter(num_alternatives__gt=0)
-        self.__majority_opinion_polls = MajorityOpinionPoll.objects.annotate(num_alternatives=Count('alternative')).filter(num_alternatives__gt=0)
+        self.polls = Poll.objects.annotate(num_alternatives=Count('alternative')).filter(num_alternatives__gt=0)
 
     def get_ordered_queryset(self, by_field: str = 'text', asc: bool = False):
         """
@@ -16,17 +14,9 @@ class ActivePollsService:
         Default: descendending order by poll text
         """
         if asc:
-            ordered_single_preference_polls = self.__single_preference_polls.order_by(by_field)
-            ordered_majority_preference_polls = self.__majority_opinion_polls.order_by(by_field)
+            return self.polls.order_by(by_field)
         else:
-            ordered_single_preference_polls = self.__single_preference_polls.order_by(f'-{by_field}')
-            ordered_majority_preference_polls = self.__majority_opinion_polls.order_by(f'-{by_field}')
-        
-        return [
-            ordered_majority_preference_polls,
-            ordered_single_preference_polls,
-            []
-        ]
+            return self.polls.order_by(f'-{by_field}')
             
 class SearchPollService:
     def search_by_id(self, id: int) -> Poll:
@@ -34,6 +24,3 @@ class SearchPollService:
         if  poll.alternative_set.count() == 0:
             raise PollWithoutAlternativesException
         return poll
-    
-    def get_alternatives_of_a_poll(self, poll: Poll) -> QuerySet[Alternative]:
-        return Alternative.objects.filter(poll=poll)
