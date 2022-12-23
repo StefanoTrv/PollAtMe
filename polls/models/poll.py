eef5327ea3d32ec2adaa81b4eb0c8080d9b06a09
Riccardo <135047@spes.uniud.it>
2022-12-23 14:27:17 +0000

    poll date added to model

--- polls/models/poll.py
+++ polls/models/poll.py
@@ -4,6 +4,8 @@ from django.db import models
 class Poll(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
+    start = models.DateTimeField(auto_now_add = False, auto_now = False)
+    end = models.DateTimeField(auto_now_add = False, auto_now = False)
 
     def __str__(self) -> str:
         return self.title
