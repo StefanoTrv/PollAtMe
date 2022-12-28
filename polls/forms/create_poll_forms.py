ab6378b206e60c4cda6af1d6f49cd851f707ddd4
Riccardo Belliato <belliatoriccardo@gmail.com>
2022-12-28 10:26:57 +0000

    nuova pagina creazione

--- polls/forms/create_poll_forms.py
+++ polls/forms/create_poll_forms.py
@@ -78,6 +78,9 @@ class CreatePollFormMain(forms.Form):
             self.add_error(None, "Il testo del sondaggio non può essere vuoto.")
 
         return form_data
+    
+    def alternatives_group(self):
+        return [self[name] for name in filter(lambda x: x.startswith('alternative'), self.fields.values())]
 
 #Form per la seconda pagina della creazione di nuovi sondaggi, contenente opzioni secondarie
 class CreatePollAdditionalOptions(forms.Form):#per ora vuota, sarà da riempire con le opzioni
