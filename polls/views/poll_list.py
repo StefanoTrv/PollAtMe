4289f505b7e4466f59bad9a847d540ab2c15f1b7
Riccardo Belliato <belliatoriccardo@gmail.com>
2023-01-04 15:45:43 +0000

    fix voto rapido con stringa arbitraria

--- polls/views/poll_list.py
+++ polls/views/poll_list.py
@@ -43,7 +43,12 @@ class VoteWithCodeView(View):
     http_method_names = ['post']
 
     def post(self, request: http.HttpRequest, *args, **kwargs):
-        code = request.POST.get('code')
-        url = reverse('polls:vote', kwargs={'id': code})
+        code = request.POST.get('code', 0)
+        try:
+            poll_id = int(code)
+        except ValueError:
+            raise http.Http404("Il sondaggio ricercato non esiste")
+
+        url = reverse('polls:vote', kwargs={'id': poll_id})
 
         return http.HttpResponseRedirect(url)
