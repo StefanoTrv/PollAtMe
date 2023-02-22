c390bef74af072ca95e0f1a46c3043686394d9be
Belliato Riccardo <31370173+rik1599@users.noreply.github.com>
2023-02-22 11:15:46 +0000

    nuove pagine spiegazione metodi

--- polls/views/help_view.py
+++ polls/views/help_view.py
@@ -1,7 +1,7 @@
 from django.views.generic.base import TemplateView
 
 class ExplanationGMView(TemplateView):
-    template_name = "polls/spiegazione_metodi_voto/spiegazione_giudizio_maggioritario.html"
+    template_name = "polls/help/spiegazione_giudizio_maggioritario.html"
 
 class HelpSimulatedResultsView(TemplateView):
-    template_name: str = 'polls/help_simulated_results.html'
+    template_name: str = 'polls/help/help_simulated_results.html'
