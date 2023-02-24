fafb10083359dedb261518a267db957134a13168
Belliato Riccardo <31370173+rik1599@users.noreply.github.com>
2023-02-24 13:33:01 +0000

    fixed grafici responsive

--- polls/views/explaining_view.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic.base import TemplateView
-
-class ExplanationGMView(TemplateView):
-    template_name = "polls/spiegazione_metodi_voto/spiegazione_giudizio_maggioritario.html"
-
-class HelpSimulatedResultsView(TemplateView):
-    template_name: str = 'polls/help_simulated_results.html'
