2fe4a96bbbd5285088c78eab652694068884e99f
Riccardo <135047@spes.uniud.it>
2023-03-24 15:09:40 +0000

    fix test

--- tests/middleware/test_revote_session_cleaner.py
+++ tests/middleware/test_revote_session_cleaner.py
@@ -4,10 +4,12 @@ from assertpy import assert_that # type: ignore
 
 class RevoteSessionCleanerTest(TestCase):
 
-    vote_url = reverse('polls:vote_MJ', args=[1])
+    fixtures = ['polls.json']
+    vote_url = reverse('polls:vote_single_preference', args=[1])
 
     def test_clears_revote(self):
-            self.client.post(self.vote_url, data= {'alternative_sp': '', 'preference_id': ''})
-            response = self.client.get(self.vote_url)
+            response = self.client.post(self.vote_url, data= {'alternative': 1})
+            self.assertEqual(response.status_code,200)
+            response = self.client.get(reverse('polls:index'))
             assert_that(response.get('preference_id') is None)
             assert_that(response.get('alternative_sp') is None)
