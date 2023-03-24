4cc7ceb2da12310aae6336ffbdfbafb85393f228
Riccardo <135047@spes.uniud.it>
2023-03-24 09:42:41 +0000

    fix test

--- tests/middleware/test_revote_session_cleaner.py
+++ tests/middleware/test_revote_session_cleaner.py
@@ -7,7 +7,7 @@ class RevoteSessionCleanerTest(TestCase):
     vote_url = reverse('polls:vote_MJ', args=[1])
 
     def test_clears_revote(self):
-            self.client.post(self.vote_url, data= {'alternative_sp': 'Stracciatella', 'preference_id': '1'})
+            self.client.post(self.vote_url, data= {'alternative_sp': '', 'preference_id': ''})
             response = self.client.get(self.vote_url)
             assert_that(response.get('preference_id') is None)
             assert_that(response.get('alternative_sp') is None)
