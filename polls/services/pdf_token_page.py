92a86f9f0a63fe81723f1e69faa0802849c09312
Riccardo Belliato <belliatoriccardo@gmail.com>
2023-04-08 11:30:53 +0000

    pagina ticket

--- polls/services/pdf_token_page.py
+++ polls/services/pdf_token_page.py
@@ -1,33 +1,54 @@
+import io
 from typing import Any
-from django.db.models import QuerySet # type: ignore
+
 from django import http
-from xhtml2pdf import pisa # type: ignore
-from django.template.loader import get_template
+from django.db.models import QuerySet  # type: ignore
+from reportlab.pdfgen import canvas  # type: ignore
+from reportlab.lib.pagesizes import A4  # type: ignore
+from reportlab.lib.units import cm  # type: ignore
 
-from polls.models import TokenizedPoll, Token
+from reportlab_qrcode import QRCodeImage # type: ignore
 
-def response_to_pdf(html):
-    response = http.HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
-    
-    pisa_status = pisa.CreatePDF(
-        html, dest=response
-    )
+from polls.models import Token, TokenizedPoll
 
-    if pisa_status.err:
-       return http.HttpResponse('We had some errors <pre>' + html + '</pre>')
-    return response
 
-def generate_pdf(poll : TokenizedPoll, scheme, host) -> http.HttpResponse:
-    template = get_template('polls/tickets.html')
-    context: dict[str, Any] = {
-        'links': [
-            f"{scheme}://{host}/{poll.mapping.code}/{token.token}/"
-            for token in poll.token_set.filter(used=False)
-        ],
-        'poll': poll,
-    }
-    html = template.render(context)
+def render_pdf(poll : TokenizedPoll, scheme, host) -> http.FileResponse:
+    buffer = io.BytesIO()
+    p = canvas.Canvas(buffer)
+    width, height = A4
+    p.grid([0, width / 2, width], [0, height / 4, height / 2, 3 * height / 4, height])
 
-    return response_to_pdf(html)
-    #return http.HttpResponse(html)
+    token = poll.token_set.filter(used=False).first()
+    link = f"{scheme}://{host}/{poll.mapping.code}/{token.token if token is not None else 'token'}"
+    qr = QRCodeImage(link, size=3.5 * cm)
+
+    qr.drawOn(p, 195, 5)
+    start_y = 190
+    start_x = 15
+    p.drawString(start_x, start_y, "Vota la mia scelta")
+    p.drawString(start_x, start_y-20, f"1) Vai sul sito:")
+    p.drawString(start_x, start_y-40, f"{scheme}://sceglimeglio.azurewebsites.net/{poll.mapping.code}")
+    p.drawString(start_x, start_y-60, f"2) Inserisci il codice:")
+    p.drawString(start_x, start_y-80, f"{token.token if token is not None else 'token'}")
+    p.drawString(start_x, start_y-100, "OPPURE")
+    p.drawString(start_x, start_y-120, "Scansiona il QR code")
+    p.drawString(start_x, start_y-150, f"Valido dal {poll.start.strftime('%d/%m/%Y %H:%M')}")
+    p.drawString(start_x, start_y-170, f"al {poll.end.strftime('%d/%m/%Y %H:%M')}")
+
+    qr.drawOn(p, 195 + (width / 2), 5 + (height / 4))
+    start_y = 190 + (height / 4)
+    start_x = 15 + (width / 2)
+    p.drawString(start_x, start_y, "Vota la mia scelta")
+    p.drawString(start_x, start_y-20, f"1) Vai sul sito:")
+    p.drawString(start_x, start_y-40, f"{scheme}://sceglimeglio.azurewebsites.net/{poll.mapping.code}")
+    p.drawString(start_x, start_y-60, f"2) Inserisci il codice:")
+    p.drawString(start_x, start_y-80, f"{token.token if token is not None else 'token'}")
+    p.drawString(start_x, start_y-100, "OPPURE")
+    p.drawString(start_x, start_y-120, "Scansiona il QR code")
+    p.drawString(start_x, start_y-150, f"Valido dal {poll.start.strftime('%d/%m/%Y %H:%M')}")
+    p.drawString(start_x, start_y-170, f"al {poll.end.strftime('%d/%m/%Y %H:%M')}")
+
+    p.showPage()
+    p.save()
+    buffer.seek(0)
+    return http.FileResponse(buffer, as_attachment=False, filename='tickets.pdf')
--- polls/views/tokens_view.py
+++ polls/views/tokens_view.py
@@ -5,18 +5,18 @@ from django.db.models import Model
 from django.views.generic.list import ListView
 
 from polls.models import Poll, TokenizedPoll
-from polls.services import SearchPollService, generate_pdf
+from polls.services import SearchPollService, render_pdf
 class TokensView(ListView):
     model: Optional[Type[Model]] = Poll
     paginate_by: int = 6
     template_name: str = 'polls/tokens_page.html'
 
-def download_tokens(request: http.HttpRequest, id: int) -> http.HttpResponse:
+def download_tokens(request: http.HttpRequest, id: int) -> http.FileResponse:
 
     poll = SearchPollService().search_by_id(id)
 
     if not isinstance(poll, TokenizedPoll):
         raise http.Http404('Poll not found')
     
-    response = generate_pdf(poll, request.scheme, request.get_host())
+    response = render_pdf(poll, request.scheme, request.get_host())
     return response
