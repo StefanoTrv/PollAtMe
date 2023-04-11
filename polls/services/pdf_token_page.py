ee17927bf7fc0402ab2e7a5f7510262933b7aac4
Riccardo Belliato <belliatoriccardo@gmail.com>
2023-04-11 13:56:41 +0000

    pagina biglietti con token

--- polls/services/pdf_token_page.py
+++ polls/services/pdf_token_page.py
@@ -1,9 +1,7 @@
 import io
-from typing import Any
 
 from django import http
 from django.core.paginator import Paginator
-from django.db.models import QuerySet
 from reportlab.lib.pagesizes import A4  # type: ignore
 from reportlab.lib.units import cm  # type: ignore
 from reportlab.pdfgen import canvas  # type: ignore
@@ -13,7 +11,6 @@ from polls.models import Token, TokenizedPoll
 
 
 class TicketGenerator:
-    buffer = io.BytesIO()
     width, height = A4
 
     TICKETS_PER_ROW = 2
@@ -21,8 +18,9 @@ class TicketGenerator:
     TICKETS_PER_PAGE = TICKETS_PER_ROW * TICKETS_PER_COLUMN
 
     def __init__(self, poll: TokenizedPoll, scheme: str, host: str) -> None:
+        self.buffer = io.BytesIO()
         self.poll = poll
-        self.base_url = f"{scheme}://{host}/{poll.mapping.code}/"
+        self.base_url = f"{scheme}://{host}/{poll.mapping.code}"
         self.p = canvas.Canvas(self.buffer)
         self.tokens = self.poll.token_set.filter(used=False)
 
@@ -30,30 +28,32 @@ class TicketGenerator:
         self.TICKET_HEIGHT = self.height / self.TICKETS_PER_COLUMN
     
     def print_ticket(self, token: Token, offset_x, offset_y) -> None:
-        link = f"{self.base_url}{token.token.replace(' ', '-')}"
+        link = f"{self.base_url}/{token.get_password_for_url()}"
         qr = QRCodeImage(link, size=3.5 * cm, border=1)
         qr.drawOn(self.p, offset_x + 195, offset_y + 5)
 
         start_x = offset_x + 15
         start_y = offset_y + 190
+        spacing = 20
+
         self.p.saveState()
         self.p.setFont("Helvetica-Bold", 16)
         self.p.drawString(start_x, start_y, "Vota la mia scelta")
 
         self.p.restoreState()
-        self.p.drawString(start_x, start_y-20, "1) Vai sul sito:")
-        self.p.drawString(start_x, start_y-40, self.base_url)
-        self.p.drawString(start_x, start_y-60, "2) Inserisci il codice:")
+        self.p.drawString(start_x, start_y-spacing, "1) Vai sul sito:")
+        self.p.drawString(start_x, start_y-(2*spacing), self.base_url)
+        self.p.drawString(start_x, start_y-(3*spacing), "2) Inserisci la password:")
 
         self.p.saveState()
         self.p.setFont("Helvetica-Bold", 13)
-        self.p.drawString(start_x, start_y-80, token.token)
+        self.p.drawString(start_x, start_y-(4*spacing), token.token)
         
         self.p.restoreState()
-        self.p.drawString(start_x, start_y-100, "OPPURE")
-        self.p.drawString(start_x, start_y-120, "Scansiona il QR code")
-        self.p.drawString(start_x, start_y-150, "Potrai votare fino al")
-        self.p.drawString(start_x, start_y-170, self.poll.end.strftime('%d/%m/%Y %H:%M'))
+        self.p.drawString(start_x, start_y-(5*spacing), "OPPURE")
+        self.p.drawString(start_x, start_y-(6*spacing), "Scansiona il QR code")
+        self.p.drawString(start_x, start_y-(7*spacing + 10), "Potrai votare fino al")
+        self.p.drawString(start_x, start_y-(8*spacing + 10), self.poll.end.strftime('%d/%m/%Y %H:%M'))
     
     def print_grid_on_page(self) -> None:
         self.p.grid(
@@ -78,4 +78,4 @@ class TicketGenerator:
             self.p.showPage()
         self.p.save()
         self.buffer.seek(0)
-        return http.FileResponse(self.buffer, as_attachment=False, filename='tickets.pdf')
+        return http.FileResponse(self.buffer, as_attachment=False, filename=f"tickets-{self.poll.pk}.pdf")
