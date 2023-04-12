9671e23bb15538b4956e904bdf837cac8c10f967
Riccardo <135047@spes.uniud.it>
2023-04-12 22:19:33 +0000

    generate tokens (incomplete)

--- polls/views/tokens_view.py
+++ polls/views/tokens_view.py
@@ -18,25 +18,47 @@ class TokensView(LoginRequiredMixin, TemplateView):
     model: Optional[Type[Model]] = Token
     template_name: str = 'polls/tokens_page.html'
 
-    def dispatch(self, request: http.HttpRequest, *args, **kwargs):
+    def dispatch(self, request, *args, **kwargs):
         self.poll = SearchPollService().search_by_id(kwargs['id'])
+        if self.poll.author != request.user:
+            raise PermissionDenied('Non è possibile accedere a questa pagina')
         return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['poll'] = self.poll
         context['object_list'] = Token.objects.filter(poll = self.poll)
+        context['usati'] = Token.objects.filter(poll = self.poll, used=True)
+        context['disponibili'] = Token.objects.filter(poll = self.poll, used=False)
         return context
     
+    def post(self, request, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        number_of_tokens = self.request.POST.get("n")
+        context['poll'] = self.poll
+        generate_token(request, self.poll, int(number_of_tokens))
+        context['object_list'] = Token.objects.filter(poll = self.poll)
+        context['usati'] = Token.objects.filter(poll = self.poll, used=True)
+        context['disponibili'] = Token.objects.filter(poll = self.poll, used=False)
+        return render(self.request, 'polls/tokens_page.html', context)
+
+
+@login_required
+def generate_token(request: http.HttpRequest, poll: TokenizedPoll, number_of_tokens: int):
+    if poll.author != request.user:
+        raise PermissionDenied('Non hai i permessi per generare i codici di questa scelta')
+    else:
+        if not isinstance(poll, TokenizedPoll):
+            raise http.Http404('Poll not found')
+        generate_tokens(poll, number_of_tokens) 
+
 
 @login_required
 def download_tokens(request: http.HttpRequest, id: int) -> http.FileResponse:
     poll = SearchPollService().search_by_id(id)
     if poll.author != request.user:
-        raise PermissionDenied('Non hai i permessi per cancellare questo sondaggio')
+        raise PermissionDenied('Non hai i permessi per scaricare i codici di questa scelta')
     else:
-        poll = SearchPollService().search_by_id(id)
-        print(poll)
         if not isinstance(poll, TokenizedPoll):
             raise http.Http404('Poll not found')
         response = TicketGenerator(
