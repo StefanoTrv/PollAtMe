7a511c126a808dd1ec6f4cf43de9907fa55e660c
Riccardo <135047@spes.uniud.it>
2023-04-12 00:00:08 +0000

    TokensView (not working)

--- polls/views/tokens_view.py
+++ polls/views/tokens_view.py
@@ -1,15 +1,37 @@
 from typing import Any, Optional, Type
 
 from django import http
-from django.db.models import Model
+from django.db.models import Model, QuerySet
 from django.views.generic.list import ListView
+from django.http import HttpResponse
 
-from polls.models import Poll, TokenizedPoll
+from polls.models import TokenizedPoll, Token
 from polls.services import SearchPollService, TicketGenerator
+from polls.services.token_generator import generate_tokens
+
+
 class TokensView(ListView):
-    model: Optional[Type[Model]] = Poll
-    paginate_by: int = 6
+    model: Optional[Type[Model]] = Token
     template_name: str = 'polls/tokens_page.html'
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.__tokens: QuerySet[Token] = [] #type: ignore
+        self.__poll = SearchPollService().search_by_id(335)
+        if not isinstance(self.__poll, TokenizedPoll):
+            raise http.Http404('Poll not found')
+
+    def generate(self, request: http.HttpRequest, id: int):
+        self.__poll = SearchPollService().search_by_id(self.kwargs['id'])
+        if not isinstance(self.__poll, TokenizedPoll):
+            raise http.Http404('Poll not found')
+        generate_tokens(self.__poll, 1)
+    
+    def get_queryset(self) -> QuerySet[Token]:
+        if not isinstance(self.__poll, TokenizedPoll):
+            raise http.Http404('Poll not found')
+        generate_tokens(self.__poll, 1)
+        return Token.objects.filter(poll = self.__poll)
 
 def download_tokens(request: http.HttpRequest, id: int) -> http.FileResponse:
 
