ed256c43ab95ccad51d77a005c4aed93cd677193
Riccardo <135047@spes.uniud.it>
2023-04-12 15:12:53 +0000

    TokensView incompleta

--- polls/views/tokens_view.py
+++ polls/views/tokens_view.py
@@ -1,48 +1,47 @@
+from ast import Dict
 from typing import Any, Optional, Type
 
 from django import http
-from django.db.models import Model, QuerySet
-from django.views.generic.list import ListView
-from django.http import HttpResponse
+from django.db.models import Model
+from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 
 from polls.models import TokenizedPoll, Token
 from polls.services import SearchPollService, TicketGenerator
 from polls.services.token_generator import generate_tokens
 
 
-class TokensView(ListView):
+class TokensView(LoginRequiredMixin, TemplateView):
     model: Optional[Type[Model]] = Token
     template_name: str = 'polls/tokens_page.html'
 
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.__tokens: QuerySet[Token] = [] #type: ignore
-        self.__poll = SearchPollService().search_by_id(335)
-        if not isinstance(self.__poll, TokenizedPoll):
-            raise http.Http404('Poll not found')
+    def dispatch(self, request: http.HttpRequest, *args, **kwargs):
+        self.poll = SearchPollService().search_by_id(kwargs['id'])
+        return super().dispatch(request, *args, **kwargs)
 
-    def generate(self, request: http.HttpRequest, id: int):
-        self.__poll = SearchPollService().search_by_id(self.kwargs['id'])
-        if not isinstance(self.__poll, TokenizedPoll):
-            raise http.Http404('Poll not found')
-        generate_tokens(self.__poll, 1)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['poll'] = self.poll
+        context['object_list'] = Token.objects.filter(poll = self.poll)
+        return context
     
-    def get_queryset(self) -> QuerySet[Token]:
-        if not isinstance(self.__poll, TokenizedPoll):
-            raise http.Http404('Poll not found')
-        generate_tokens(self.__poll, 1)
-        return Token.objects.filter(poll = self.__poll)
 
+@login_required
 def download_tokens(request: http.HttpRequest, id: int) -> http.FileResponse:
-
     poll = SearchPollService().search_by_id(id)
-
-    if not isinstance(poll, TokenizedPoll):
-        raise http.Http404('Poll not found')
-    
-    response = TicketGenerator(
-        poll, 
-        request.scheme if request.scheme is not None else "", 
-        request.get_host()
-        ).render()
-    return response
+    if poll.author != request.user:
+        raise PermissionDenied('Non hai i permessi per cancellare questo sondaggio')
+    else:
+        poll = SearchPollService().search_by_id(id)
+        print(poll)
+        if not isinstance(poll, TokenizedPoll):
+            raise http.Http404('Poll not found')
+        response = TicketGenerator(
+            poll, 
+            request.scheme if request.scheme is not None else "", 
+            request.get_host()
+            ).render()
+        return response
