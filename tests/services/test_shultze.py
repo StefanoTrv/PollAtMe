e0fede6b7e668e347fc94b26b1d3a54b999db946
Riccardo Belliato <belliatoriccardo@gmail.com>
2023-05-10 15:00:19 +0000

    test per shultze

---
+++ tests/services/test_shultze.py
@@ -0,0 +1,55 @@
+from django.test import TestCase
+
+class TestShultze(TestCase):
+
+    def test_calculate_occurrences(self):
+        pass
+
+    def test_preference_matrix(self):
+        input = {
+            ('A', 'C', 'B', 'E', 'D'): 5,
+            ('A', 'D', 'E', 'C', 'B'): 5,
+            ('B', 'E', 'D', 'A', 'C'): 8,
+            ('C', 'A', 'B', 'E', 'D'): 3,
+            ('C', 'A', 'E', 'B', 'D'): 7,
+            ('C', 'B', 'A', 'D', 'E'): 2,
+            ('D', 'C', 'E', 'B', 'A'): 7,
+            ('E', 'B', 'A', 'D', 'C'): 8
+        }
+
+        expected = [
+            [0, 20, 26, 30, 22],
+            [25, 0, 16, 33, 18],
+            [19, 29, 0, 17, 24],
+            [15, 12, 28, 0, 14],
+            [23, 27, 21, 31, 0]
+        ]
+    
+    def test_widest_paths(self):
+        input = [
+            [0, 20, 26, 30, 22],
+            [25, 0, 16, 33, 18],
+            [19, 29, 0, 17, 24],
+            [15, 12, 28, 0, 14],
+            [23, 27, 21, 31, 0]
+        ]
+
+        expected = [
+            [0, 28, 28, 30, 24],
+            [25, 0, 28, 33, 24],
+            [25, 29, 0, 29, 24],
+            [25, 28, 28, 0, 24],
+            [25, 28, 28, 31, 0]
+        ]
+    
+    def test_winner(self):
+        input = [
+            [0, 28, 28, 30, 24],
+            [25, 0, 28, 33, 24],
+            [25, 29, 0, 29, 24],
+            [25, 28, 28, 0, 24],
+            [25, 28, 28, 31, 0]
+        ]
+
+        # Indici delle righe coi vincitori
+        expected = (4)
