4b52731ab1952092fd8f3223145eb64a5079c2d9
Riccardo <135047@spes.uniud.it>
2023-05-17 16:58:17 +0000

    modal rivoto non perfettamente funzionante

--- polls/views/vote_view.py
+++ polls/views/vote_view.py
@@ -136,6 +136,7 @@ class VoteSinglePreferenceView(_VoteView):
                 new_preference)
             self.request.session['preference_id'] = synthetic_preference.id
             self.request.session['alternative_sp'] = new_preference.alternative.text
+            self.request.session['revote_type'] = 'single_preference'
             return render(self.request, 'polls/vote_success.html', {'poll': self.poll, 'revote': True, 'token': self.token})
 
         return render(self.request, 'polls/vote_success.html', {'poll': self.poll})
@@ -202,6 +203,7 @@ class VoteShultzeView(_VoteView):
         if self.poll.get_type() == self.pollType:
             syntethic_preference = pref.MajorityPreference.save_mj_from_shultze(preference)
             self.request.session['preference_id'] = syntethic_preference.id
+            self.request.session['revote_type'] = 'schulze_method'
             self.request.session['sequence_shultze'] = [alt.alternative.text for alt in preference.shultzeopinionjudgement_set.order_by('order').all()]
             return render(self.request, 'polls/vote_success.html', {'poll': self.poll, 'revote': True, 'token': self.token})
 
